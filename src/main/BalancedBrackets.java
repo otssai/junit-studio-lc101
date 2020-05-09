e0f368303e5367424ef4d72996a0a8a3791c8205
Xiang Yu <yuxiang@Xiangs-MacBook-Air.local>
2020-05-08 22:11:26 -0500

    error fixed

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -24,6 +24,11 @@ public class BalancedBrackets {
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
         for (char ch : str.toCharArray()) {
+
+            if (brackets < 0) {
+                return false;
+            }
+
             if (ch == '[') {
                 brackets++;
             } else if (ch == ']') {
